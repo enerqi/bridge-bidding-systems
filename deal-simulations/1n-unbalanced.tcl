723d6a6021bef4cc2a1c3c19a3397d79d2751766
enerqi <enerqi>
2021-03-15 23:24:46 +0000

    Many more deal simulations

---
+++ deal-simulations/1n-unbalanced.tcl
@@ -0,0 +1,11 @@
+# Run with: deal.exe [-v] -i path/to/scratch.tcl n
+# The TCL interpreter annoyingly needs forward slashes in windows paths
+# The main `deal.exe` program expects to run with the working directory matching the deal.exe directory
+# http://bridge.thomasoandrews.com/deal30/
+set script_path [ file dirname [ file normalize [ info script ] ] ]
+source $script_path/deal-utils.tcl
+
+main {
+  if {[is_1nt_opener north] && ![flattish south]} { accept }
+  reject
+}
