91217a2e4eb3e6cf2cdc1da92b5925e0a0e2361e
enerqi <enerqi>
2021-03-28 19:55:23 +0100

    Strong club defence simulation

--- deal-simulations/defence-vs-strong-club-unbal-or-major.tcl
+++ deal-simulations/defence-vs-strong-club-unbal-or-major.tcl
@@ -6,6 +6,12 @@ set script_path [ file dirname [ file normalize [ info script ] ] ]
 source $script_path/deal-utils.tcl
 
 main {
-    if {[5CM_nt east 17 19] || [is_strong_1c east]} { accept }
+    if {![5CM_nt east 17 19] && ![is_strong_1c east]} { reject }
+
+    if {[hcp south]>=10 && [majors_4_4 south]} { accept }
+    if {[hcp south]>8 && ![flattish south]} { accept }
+    if {[hcp south]>8 && [has_9_plus_majors south]} { accept }
+    if {[hcp south]>5 && [two_suiter south]} { accept }
+
     reject
 }
