1521276add5b7f613b0548cc8aa4309252fc0c6f
enerqi <enerqi>
2023-10-01 13:38:48 +0100

    few simulations

---
+++ deal-simulations/unbalanced-intermediate-two-suiter-overcall.tcl
@@ -0,0 +1,16 @@
+# Run with: deal.exe [-v] -i path/to/scratch.tcl n
+# The TCL interpreter annoyingly needs forward slashes in windows paths
+# The main `deal.exe` program expects to run with the working directory matching the deal.exe directory
+# http://bridge.thomasoandrews.com/deal30/
+set script_path [ file dirname [ file normalize [ info script ] ] ]
+source $script_path/deal-utils.tcl
+
+main {
+    # east opens
+    if {[flattish east] || [hcp east]< 12 || [hcp east] >19} { reject }
+
+    set points [hcp south]
+    if {$points < 11 || $points > 15} { reject }
+    if {[two_suiter south]} { accept }
+    reject
+}
